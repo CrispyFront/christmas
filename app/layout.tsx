14144869a390076cf138565b4584eb396e9407a9
hanseulhee <3021062@gmail.com>
2023-11-24 14:30:51 +0900

    init: next.js 14 settings

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Christmas Test",
+  description: "☃️ 크리스마스에 뭐하지?",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>크리스마스에 모 ~ 하냐</main>;
+}
