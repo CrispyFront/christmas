fe74e8f4136135716c1d985ee463475788d4236e
hanseulhee <3021062@gmail.com>
2023-11-26 22:08:57 +0900

    [#2] delete metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,6 @@
 import GoogleAnalytics from "components/tracking/GoogleAnalytics";
 import Hotjar from "components/tracking/Hotjar";
 import useGAPageView from "hooks/useGAPageView";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Christmas Test",
-  description: "☃️ 크리스마스에 뭐하지?",
-};
 
 export default function RootLayout({
   children,
