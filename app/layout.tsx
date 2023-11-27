0e2076f51983183de5ad125b64e926ad84dad415
Archer <sp38013@naver.com>
2023-11-27 12:29:11 +0900

    [#4]fix :resolved  git conflicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,21 @@
-"use client";
-
-import GoogleAnalytics from "components/tracking/GoogleAnalytics";
-import Hotjar from "components/tracking/Hotjar";
-import useGAPageView from "hooks/useGAPageView";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import React from "react";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Christmas Test",
+  description: "☃️ 크리스마스에 뭐하지?",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useGAPageView();
-
   return (
     <html lang="ko">
-      <body>
-        <GoogleAnalytics />
-        <Hotjar />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
