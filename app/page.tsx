b42afbb78e23fde5130b85774a7af2b347e65444
Archer <sp38013@naver.com>
2023-11-28 20:44:38 +0900

    [#28] refactor : storybook js to tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import SharerKaKao from "components/Button/SharerKakao";
+
 function Home() {
-  return <main>크리스마스에 모 ~ 하냐</main>;
+  return;
+  <>
+    <main>크리스마스에 모 ~ 하냐</main>
+    <SharerKaKao />
+  </>;
 }
 
 export default Home;
