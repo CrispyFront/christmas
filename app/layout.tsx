5030863cfe0ebc4bcb2624a547643566a8f3c029
Archer <sp38013@naver.com>
2023-11-28 17:47:20 +0900

    [#22]feat : Add Meta components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import GoogleAnalytics from "components/tracking/GoogleAnalytics";
 import Hotjar from "components/tracking/Hotjar";
 import useGAPageView from "hooks/useGAPageView";
 import Layout from "components/layout/Layout";
+import Meta from "components/commons/Meta";
 import "globals.css";
 import React from "react";
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 
   return (
     <html lang="ko">
+      <head>
+        <Meta />
+      </head>
       <body>
         <GoogleAnalytics />
         <Hotjar />
