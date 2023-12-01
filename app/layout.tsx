2861ece7c4893a498dbf088eb9a679938e01e733
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-01 18:30:54 +0900

    [#34] env: Change order import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import React from "react";
 import Meta from "components/commons/Meta";
 import Layout from "components/layout/Layout";
 import GoogleAnalytics from "components/tracking/GoogleAnalytics";
 import Hotjar from "components/tracking/Hotjar";
 import useGAPageView from "hooks/useGAPageView";
-import React from "react";
 import GlobalStyle from "styles/GlobalStyle";
 
 export default function RootLayout({
