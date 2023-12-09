147c5e71f5d4f98be5fb581c361d2f7935650f10
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-09 18:50:15 +0900

    [#62] feat : Setting MongoDB Add Schema

---
+++ libs/db/DBConnect.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+declare global {
+  var mongoose: any;
+}
+
+const MONGODB_URI: string = process.env.NEXT_PUBLIC_MONGODB_URI ?? "";
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
