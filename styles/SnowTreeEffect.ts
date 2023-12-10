98c7085cf07381e0f3e4f37d9b708fdc1926aa01
oauch <lgm9515@naver.com>
2023-12-10 16:13:39 +0900

    refactor: Effect name change

--- styles/SnowTreeEffect.ts
+++
@@ -1,5 +0,0 @@
-export const SnowTreeEffect = {
-  initial: { x: -200 },
-  animate: { x: 200 },
-  transition: { duration: 1 },
-};
---
+++ styles/SpringEffect.ts
@@ -0,0 +1,30 @@
+export const SpringEffect = {
+  initial: {
+    opacity: 0,
+    scale: 0.5,
+    transition: {
+      duration: 0.3,
+      ease: [0, 0.71, 0.2, 1.01],
+      scale: {
+        type: "spring",
+        damping: 7,
+        stiffness: 80,
+        restDelta: 0.001,
+      },
+    },
+  },
+  animate: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.3,
+      ease: [0, 0.71, 0.2, 1.01],
+      scale: {
+        type: "spring",
+        damping: 7,
+        stiffness: 80,
+        restDelta: 0.001,
+      },
+    },
+  },
+};
