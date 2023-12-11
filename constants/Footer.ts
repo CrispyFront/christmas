8f3e1838030e0c7ec1c0ca745ca3f0b847717f73
oauch <lgm9515@naver.com>
2023-12-11 19:43:16 +0900

    feat: email, instagram url constant add

---
+++ constants/Footer.ts
@@ -0,0 +1,5 @@
+export const URL = {
+  email:
+    "mailto:forchristmastest@gmail.com?subject=[크리스마스에 뭐하지] 문의&body=문의 내용: %0D%0A%0D%0A기타 문의: ",
+  instagram: "https://www.instagram.com/christmas_test/",
+};
