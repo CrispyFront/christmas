560f6dc74f38750966b7ad5efbef6f97460a6126
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-11 21:42:43 +0900

    [#69] feat : Return Component

---
+++ components/Button/Return.tsx
@@ -0,0 +1,21 @@
+"use client";
+import styled from "styled-components";
+import { useRouter } from "next/navigation";
+function Return() {
+  const router = useRouter();
+
+  return <StyledReturn onClick={() => router.push("/")}>Return</StyledReturn>;
+}
+export default Return;
+
+const StyledReturn = styled.button`
+  border: 1px solid #468259;
+  border-radius: 15px;
+  font-size: 30px;
+  padding: 10px;
+  color: #468259;
+
+  &:hover {
+    background-color: #ffffff;
+  }
+`;
