70483b1c3809b60d793f1da594ea6a9384cb1cff
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-13 00:11:29 +0900

    Style : Style Share Facebook Button

--- components/Share/ShareFacebook.tsx
+++ components/Share/ShareFacebook.tsx
@@ -1,12 +1,40 @@
 import { BASE_URL } from "constants/url";
-
+import LogoFacebook from "assets/icons/LogoFacebook.png";
+import styled from "styled-components";
+import Image from "next/image";
 //TODO: 페이스북 버튼 커스텀 이미지 넣기
-function ShareFacebook() {
+export interface Size {
+  size: "small" | "medium" | "large";
+}
+
+function ShareFacebook({ size }: Size) {
   const handleClick = () => {
     window.open(`https://www.facebook.com/sharer/sharer.php?u=${BASE_URL}`);
   };
 
-  return <button onClick={handleClick}>페이스북 공유하기</button>;
+  return (
+    <>
+      <StyledWrapper onClick={handleClick} size={size}>
+        <Image fill src={LogoFacebook} alt="페이스북 공유" />
+      </StyledWrapper>
+    </>
+  );
 }
 
 export default ShareFacebook;
+
+const StyledWrapper = styled.button<Size>`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+
+  width: ${({ size }) =>
+    size === "large" ? "80px" : size === "medium" ? "60px" : "40px"};
+  height: ${({ size }) =>
+    size === "large" ? "80px" : size === "medium" ? "60px" : "40px"};
+  border-radius: 50%;
+  background-color: #3b5998;
+  border: 0;
+  cursor: pointer;
+`;
