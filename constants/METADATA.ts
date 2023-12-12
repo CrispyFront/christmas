56b0d28e823d424a7628f1e380cf3e12d9bb8407
oauch <lgm9515@naver.com>
2023-12-12 10:39:33 +0900

    fix: METADATA IMAGE error fix

--- constants/METADATA.ts
+++ constants/METADATA.ts
@@ -3,7 +3,7 @@ import MainLogo from "assets/icons/MainLogo.png";
 const METADATA = {
   TITLE: "Christmas Test",
   DESCRIPTION: "얼마 남지 않은 크리스마스 무엇을 하고있을지 궁금하지않나요?",
-  IMAGE: MainLogo,
+  IMAGE: MainLogo.src,
   URL: "https://christmas-mocha.vercel.app/",
 };
 
