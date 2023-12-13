c04dc933d9edf6eadd2570dcad51f3a14159d7a9
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-13 10:59:59 +0900

    [#78]feat:Result Page ShareButtons

---
+++ components/Share/ShareButtons/index.tsx
@@ -0,0 +1,31 @@
+import CopyUrl from "@/components/CopyUrl";
+import ShareFacebook from "@/components/Share/ShareFacebook";
+import ShareKakao from "@/components/Share/ShareKakao";
+import Image from "next/image";
+import LogoCopay from "assets/icons/LogoCopy.png";
+import styled from "styled-components";
+
+interface ShareContent {
+  url: string;
+}
+
+function ShareButtons({ url }: ShareContent) {
+  return (
+    <StyledButtonWrapper>
+      <CopyUrl url={url}>
+        <button>
+          <Image src={LogoCopay} width={40} height={40} alt="링크복사" />
+        </button>
+      </CopyUrl>
+      <ShareFacebook size="small" />
+      <ShareKakao size="small" />
+    </StyledButtonWrapper>
+  );
+}
+
+export default ShareButtons;
+
+const StyledButtonWrapper = styled.div`
+  display: flex;
+  gap: 30px;
+`;
