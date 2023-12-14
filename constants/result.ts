5838709e2dc321b339cf1aeeeba44a3c618a2c52
hanseulhee <3021062@gmail.com>
2023-12-15 01:41:29 +0900

    [#99] feat: apply result constants

---
+++ constants/result.ts
@@ -0,0 +1,66 @@
+const RESULT_IMAGE = [
+  {
+    title: "독불장군 눈사람",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/d4349904-21d1-45db-bfed-47f76783de79",
+  },
+  {
+    title: "조공폭발 김산타",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/5140b4e1-4c13-4048-ac8e-2b1023be46c9",
+  },
+  {
+    title: "루돌프인 척하는 북극곰",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/2e8a8d91-bbf6-4753-a353-1523ec6703e8",
+  },
+  {
+    title: "난죽택 마시멜로",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/145cc784-7ad3-4b00-9e8f-df5620ccaf6d",
+  },
+  {
+    title: "자아도취 리스",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/98f9a47d-39d0-4018-90a4-3bfe2f2ab8a9",
+  },
+  {
+    title: "아낌없이 주는 트리",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/87419c0f-3708-4f29-9b28-7a6683fbdfc9",
+  },
+  {
+    title: "인생을 즐기는 진저맨",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/53d71a64-841e-48a7-a806-397d7bb4cd33",
+  },
+  {
+    title: "속으로 침 뱉는 미스터 루",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/7ae00a93-2dae-4224-8e48-17a9e1d2a04f",
+  },
+  {
+    title: "팩트폭격기 귤",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/2dd214ca-535e-4eb7-bdc0-564d15351ed0",
+  },
+  {
+    title: "굴뚝 속의 고독한 선물",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/59d8f3d7-48f1-4010-a54c-8c7df1d0f014",
+  },
+  {
+    title: "고래 싸움에 새우등 터지는 썰매",
+    img: "",
+  },
+  {
+    title: "짠돌이 산타",
+    img: "",
+  },
+  {
+    title: "괴짜 과학자 산타",
+    img: "",
+  },
+  {
+    title: "사이비교주 모닥불",
+    img: "",
+  },
+  {
+    title: "정보통 징글벨",
+    img: "https://github.com/ChristmasTest/christmas/assets/63100352/b4fee5bd-0cd8-43fd-8fa9-b15cf7343883",
+  },
+  {
+    title: "오지라퍼 도둑",
+    img: "",
+  },
+];
