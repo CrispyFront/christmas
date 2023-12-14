ec8cffc10ac520c995a313914f83461d13bc2b05
hanseulhee <3021062@gmail.com>
2023-12-15 01:19:11 +0900

    [#97] feat: modify url

--- constants/METADATA.ts
+++ constants/METADATA.ts
@@ -1,10 +1,11 @@
+import { BASE_URL } from "@/constants/url";
 import MainLogo from "assets/icons/MainLogo.png";
 
 const METADATA = {
   TITLE: "Christmas Test",
   DESCRIPTION: "얼마 남지 않은 크리스마스 무엇을 하고있을지 궁금하지않나요?",
   IMAGE: MainLogo.src,
-  URL: "https://christmas-mocha.vercel.app/",
+  URL: BASE_URL,
   LOCALE: "ko_KR",
 };
 
