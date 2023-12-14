9895ecc5fd83d01cc715f482898fee71f0fbae05
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-14 23:31:11 +0900

    [#89] fix : Build Loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,31 +1,27 @@
-"use client";
 import LoadingSpinner from "assets/images/LoadingSpinner.gif";
 import Image from "next/image";
-import styled from "styled-components";
 
 function Loading() {
+  const StyleDiv: React.CSSProperties = {
+    position: "fixed",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100vh",
+  };
+  const StyleP: React.CSSProperties = {
+    color: "#468259",
+    fontSize: "2.5rem",
+  };
+
   return (
-    <StyledWrapper>
+    <div style={StyleDiv}>
       <Image src={LoadingSpinner} alt="로딩 스피너" />
-      <StyledLoadingMessage>Loading...</StyledLoadingMessage>
-    </StyledWrapper>
+      <p style={StyleP}>Loading...</p>
+    </div>
   );
 }
 
 export default Loading;
-
-const StyledWrapper = styled.div`
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  width: 100%;
-  height: 100vh;
-`;
-
-const StyledLoadingMessage = styled.p`
-  color: #ffffff;
-  font-size: 40px;
-`;
