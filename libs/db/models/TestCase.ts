9a858398235fda53f8ee2f614dd96eae5fb237aa
sooyeong <sooyeong@sooyeongui-MacBookPro.local>
2023-12-16 23:57:39 +0900

    [#105] refactor : Modify Test Schema

--- libs/db/models/TestCase.ts
+++ libs/db/models/TestCase.ts
@@ -1,13 +1,26 @@
 import mongoose from "mongoose";
 
+interface Answer {
+  firstAnswer: string;
+  secondAnswer: string;
+}
+
 export interface Test extends mongoose.Document {
   type: string;
   question: string;
-  firstAnswer: string;
-  secondAnswer: string;
+  firstAnswer: Answer;
+  secondAnswer: Answer;
   rowState: number;
   imageURL: string;
 }
+const AnswerSchema = new mongoose.Schema({
+  type: {
+    type: String,
+  },
+  content: {
+    type: String,
+  },
+});
 
 const TestSchema = new mongoose.Schema<Test>(
   {
@@ -20,14 +33,8 @@ const TestSchema = new mongoose.Schema<Test>(
       type: String,
       default: "",
     },
-    firstAnswer: {
-      type: String,
-      default: "",
-    },
-    secondAnswer: {
-      type: String,
-      default: "",
-    },
+    firstAnswer: AnswerSchema,
+    secondAnswer: AnswerSchema,
     rowState: {
       type: Number,
       enum: [0, 1, 2], //0 : use , 1 : hide , 2 : delete
