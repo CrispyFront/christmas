4c36a05e66dba1ad797ffe26fe76840537597e81
Soo NamGoong <rndtn6851@gmail.com>
2023-12-17 13:54:33 +0900

    [#109] feat: test content logic

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,20 +5,67 @@ import Answer from "components/Button/Answer";
 import Gauge from "components/Gauge/Gauge";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
+import { getByTypeTestCase } from "@/libs/api";
+
+interface TestType {
+  type: string;
+  question: string;
+  firstAnswer: {
+    type: string;
+    content: string;
+  };
+  secondAnswer: {
+    type: string;
+    content: string;
+  };
+  imageURL: string;
+}
 
 function Test() {
+  const [tests, setTests] = useState<TestType[]>([]);
+
   const PageNum = Number(useSearchParams().get("page"));
   const ProgressBar = Array(6).fill("full");
+  const TypeNum = Math.ceil(PageNum / 4);
 
   const full: number = Math.floor(PageNum / 2);
   const half: number = PageNum % 2;
+
   if (half === 1) {
     ProgressBar[full] = "half";
     ProgressBar.fill("empty", full + 1, 6);
   } else {
     ProgressBar.fill("empty", full, 6);
   }
+
+  const fetchData = async () => {
+    let type = "";
+    switch (TypeNum) {
+      case 0:
+        type = "IE";
+        break;
+      case 1:
+        type = "PJ";
+        break;
+      case 2:
+        type = "NS";
+        break;
+      case 3:
+        type = "FT";
+        break;
+    }
+    const res = await getByTypeTestCase({ type });
+    const { typeTestCase } = res;
+    if (typeTestCase && typeTestCase.length > 0) {
+      setTests(typeTestCase);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <StyledWrapper>
@@ -27,16 +74,23 @@ function Test() {
           <Gauge key={index} type={value} />
         ))}
       </StyledProgressBar>
-      <StyledTest>
-        <Image src={MainIcon} alt="아이콘" width="300" />
-        <StyledQuestion>
-          당신은... 크리스마스 날 벽난로에서 야생의 산타와 마주쳤습니다.
-        </StyledQuestion>
-      </StyledTest>
-      <StyledButton>
-        <Answer color="green" text="1번 답" />
-        <Answer color="red" text="2번 답" />
-      </StyledButton>
+      {tests.length > 0 && (
+        <StyledTest>
+          <Image
+            src={tests[PageNum % 4].imageURL}
+            alt="아이콘"
+            width="300"
+            height="250"
+          />
+          <StyledQuestion>{tests[PageNum % 4].question}</StyledQuestion>
+        </StyledTest>
+      )}
+      {tests.length > 0 && (
+        <StyledButton>
+          <Answer color="green" text={tests[PageNum % 4].firstAnswer.content} />
+          <Answer color="red" text={tests[PageNum % 4].secondAnswer.content} />
+        </StyledButton>
+      )}
     </StyledWrapper>
   );
 }
