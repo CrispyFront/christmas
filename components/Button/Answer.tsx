a3d82ea2b86becb5ada521e8535837a0b7aa4d59
Soo NamGoong <rndtn6851@gmail.com>
2023-12-17 16:30:41 +0900

    [#109] refactor: answer component - take out onClick props

--- components/Button/Answer.tsx
+++ components/Button/Answer.tsx
@@ -7,9 +7,10 @@ import Image from "next/image";
 interface Answer {
   color: "red" | "green";
   text: string;
+  onClick: any;
 }
 
-function Answer({ color, text }: Answer) {
+function Answer({ color, text, onClick }: Answer) {
   const moveNext = () => {
     console.log(color);
   };
@@ -20,7 +21,7 @@ function Answer({ color, text }: Answer) {
   };
 
   return (
-    <StyledWrapper onClick={moveNext}>
+    <StyledWrapper onClick={onClick}>
       <Image fill src={ButtonImg[color]} alt="답변 버튼" />
       <p>{text}</p>
     </StyledWrapper>
